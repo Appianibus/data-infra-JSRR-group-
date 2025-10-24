1335cde002428da9f2f5d9a3a73b843069b95604
Appianibus <154466700+Appianibus@users.noreply.github.com>
2025-10-24 10:19:24 +0200

    Update main.py

--- 04-Metadata-Extraction-and-Visualization/main.py
+++ 04-Metadata-Extraction-and-Visualization/main.py
@@ -1,1 +1,18 @@
-def main()
+from semmeta.metadata_extractor_module import SEMMetaData
+from semmeta.json_cleaner_module import JSONReader
+from semmeta.visualizer_module import Visualizer
+def main():
+    image_path = input("Provide image path:")
+
+    semmetadata = SEMMetaData(image_path)
+    json_path = semmetadata.GetInsMetadata()
+
+    cleaner = JSONReader(json_path, json_path)
+
+    cleaner.clean()
+    cleaner.save()
+
+    visualizer = Visualizer(json_path, image_path)
+
+    visualizer.show_image()
+    visualizer.display_table()
